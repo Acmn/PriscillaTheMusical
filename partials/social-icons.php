e1493442af0cf6ae629b80900f7a3d728dc1b489
mtedwards <matt@emptyhead.com.au>
2015-11-03 19:01:39 +1100

    Unsubmitted changes

--- partials/social-icons.php
+++ partials/social-icons.php
@@ -1,31 +1,31 @@
 
 		  <?php 
 		    if(get_field('facebook_url','options')) { ?>
-		    <a class="facebook" href="<?php the_field('facebook_url','options'); ?>" target="_blank"><i class="fa fa-facebook"></i></a>
+		    <a class="social-icon facebook" data-social="facebook" href="<?php the_field('facebook_url','options'); ?>" target="_blank"><i class="fa fa-facebook"></i></a>
 		  <?php 
 		    }
 		      
 		    if(get_field('twitter_url','options')) {
 		  ?>
-		    <a class="twitter" href="<?php the_field('twitter_url','options'); ?>" target="_blank"><i class="fa fa-twitter"></i></a>
+		    <a class="social-icon twitter" data-social="twitter" href="<?php the_field('twitter_url','options'); ?>" target="_blank"><i class="fa fa-twitter"></i></a>
 		  <?php 
 		    }
 		      
 		    if(get_field('instagram_url','options')) {
 		  ?>
-		    <a class="instagram" href="<?php the_field('instagram_url','options'); ?>" target="_blank"><i class="fa fa-instagram"></i></a>
+		    <a class="social-icon instagram" data-social="instagram" href="<?php the_field('instagram_url','options'); ?>" target="_blank"><i class="fa fa-instagram"></i></a>
 		  <?php 
 		    }
 		      
 		    if(get_field('youtube_url','options')) {
 		  ?>
-		    <a class="youtube" href="<?php the_field('youtube_url','options'); ?>" target="_blank"><i class="fa fa-youtube"></i></a>
+		    <a class="social-icon youtube" data-social="youtube" href="<?php the_field('youtube_url','options'); ?>" target="_blank"><i class="fa fa-youtube"></i></a>
 		  <?php 
 		    }
 		      
 		    if(get_field('gplus_url','options')) {
 		  ?>
-		    <a class="gplus" href="<?php the_field('gplus_url','options'); ?>" target="_blank"><i class="fa fa-google-plus"></i></a>
+		    <a class="social-icon gplus" data-social="googlePlus" href="<?php the_field('gplus_url','options'); ?>" target="_blank"><i class="fa fa-google-plus"></i></a>
 		  <?php 
 		    }
 		  ?>
